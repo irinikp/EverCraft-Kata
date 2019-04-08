4f006daac71d7bef3ff099c738d0062c31b0459e
Irini Koutaki <irinikp@gmail.com>
2019-04-08 18:48:12 +0300

    Character can attack

---
+++ src/CombatAction.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Dnd;
+
+/**
+ * Class CombatAction
+ * @package Dnd
+ */
+class CombatAction
+{
+    /**
+     * @var Character
+     */
+    protected $attacker;
+    /**
+     * @var Character
+     */
+    protected $target;
+
+    /**
+     * CombatAction constructor.
+     *
+     * @param Character $attacker
+     * @param Character $target
+     */
+    public function __construct($attacker, $target)
+    {
+        $this->attacker = $attacker;
+        $this->target   = $target;
+    }
+
+    /**
+     * @return bool
+     */
+    public function attackRoll()
+    {
+        $dice = $this->attacker->roll(20);
+        return $this->hits($dice);
+    }
+
+    /**
+     * @param int $dice
+     *
+     * @return bool
+     */
+    public function hits($dice)
+    {
+        return $dice >= $this->target->getAc();
+    }
+}
--- tests/TestIteration1.php
+++ tests/TestIteration1.php
@@ -5,13 +5,15 @@ namespace Tests;
 require __DIR__ . '/../vendor/autoload.php';
 
 use Dnd\Alignment;
+use Dnd\Battle;
 use Dnd\Character;
+use Dnd\CombatAction;
 
 class TestIteration1 extends \PHPUnit\Framework\TestCase
 {
     protected $character;
 
-    public function setUp() : void
+    public function setUp(): void
     {
         parent::setUp();
         $this->character = new Character();
@@ -47,10 +49,49 @@ class TestIteration1 extends \PHPUnit\Framework\TestCase
 
     public function test_roll_dice()
     {
-        for ($i=1; $i<=20; $i++) {
+        for ($i = 1; $i <= 20; $i++) {
             $dice = $this->character->roll($i);
             $this->assertGreaterThanOrEqual(1, $dice);
             $this->assertLessThanOrEqual($i, $dice);
         }
+    }
+
+    public function test_attack_roll_1()
+    {
+        $hits = $this->create_mock_attack_roll(1);
+        $this->assertEquals(false, $hits);
+    }
+
+    public function test_attack_roll_5()
+    {
+        $hits = $this->create_mock_attack_roll(5);
+        $this->assertEquals(false, $hits);
+    }
+
+    public function test_attack_roll_10()
+    {
+        $hits = $this->create_mock_attack_roll(10);
+        $this->assertEquals(true, $hits);
+    }
+
+    public function test_attack_roll_15()
+    {
+        $hits = $this->create_mock_attack_roll(15);
+        $this->assertEquals(true, $hits);
+    }
+
+    public function test_attack_roll_20()
+    {
+        $hits = $this->create_mock_attack_roll(20);
+        $this->assertEquals(true, $hits);
+    }
+
+    private function create_mock_attack_roll($dice)
+    {
+        $character = \Mockery::mock(Character::class);
+
+        $character->shouldReceive('roll')->withArgs([20])->once()->andReturn($dice);
+        $action = new CombatAction($character, new Character());
+        return $action->attackRoll();
     }
 }
