141c95d7b8fb4b37af9891a3d8094bcf7f77133f
Irini Koutaki <irinikp@gmail.com>
2019-04-19 13:04:06 +0300

    set terrain quality

--- Bonus_Iteration/src/BattleGrid/BattleGrid.php
+++ Bonus_Iteration/src/BattleGrid/BattleGrid.php
@@ -24,7 +24,7 @@ class BattleGrid
      */
     public function __construct()
     {
-        $this->map = array();
+        $this->map = [];
     }
 
     /**
@@ -48,30 +48,23 @@ class BattleGrid
     }
 
     /**
-     * @param int $x
-     * @param int $y
-     */
-    protected function setTerrain($x, $y): void {
-        $this->map = array(array_fill(0, 20, new Terrain()), array_fill(0, 20, new Terrain()));
-        for ($i=0; $i<$x; ++$i) {
-            for ($j = 0; $j<$y; ++$j) {
-                $this->map[$i][$j] = new Terrain();
-            }
-        }
-    }
-
-    /**
-     * @param string $height
+     * @param string     $height
      * @param Dimensions $from
      * @param Dimensions $to
      */
     public function setTerrainHeight($height, Dimensions $from, Dimensions $to): void
     {
-        for ($x = $from->getX(); $x<= $to->getX(); ++$x) {
-            for ($y = $from->getY(); $y<= $to->getY(); ++$y) {
-                $this->map[$x][$y]->setHeight($height);
-            }
-        }
+        $this->setTerrainCharacteristic($height, 'Height', $from, $to);
+    }
+
+    /**
+     * @param string     $quality
+     * @param Dimensions $from
+     * @param Dimensions $to
+     */
+    public function setTerrainQuality($quality, Dimensions $from, Dimensions $to): void
+    {
+        $this->setTerrainCharacteristic($quality, 'Quality', $from, $to);
     }
 
     /**
@@ -84,5 +77,49 @@ class BattleGrid
     {
         return $this->map[$x][$y]->getHeight();
     }
+
+    /**
+     * @param int $x
+     * @param int $y
+     *
+     * @return string
+     */
+    public function getTerrainQuality($x, $y): string
+    {
+        return $this->map[$x][$y]->getQuality();
+    }
+
+    /**
+     * @param int $x
+     * @param int $y
+     */
+    protected function setTerrain($x, $y): void
+    {
+        $this->map = [array_fill(0, 20, new Terrain()), array_fill(0, 20, new Terrain())];
+        for ($i = 0; $i < $x; ++$i) {
+            for ($j = 0; $j < $y; ++$j) {
+                $this->map[$i][$j] = new Terrain();
+            }
+        }
+    }
+
+    /**
+     * @param string     $characteristic
+     * @param string     $type
+     * @param Dimensions $from
+     * @param Dimensions $to
+     */
+    protected function setTerrainCharacteristic($characteristic, $type, Dimensions $from, Dimensions $to): void
+    {
+        if ('Quality' === $type || 'Height' === $type) {
+            $function = 'set' . $type;
+            for ($x = $from->getX(); $x <= $to->getX(); ++$x) {
+                for ($y = $from->getY(); $y <= $to->getY(); ++$y) {
+                    $this->map[$x][$y]->$function($characteristic);
+                }
+            }
+        }
+    }
+
 
 }
--- Bonus_Iteration/src/BattleGrid/Terrain.php
+++ Bonus_Iteration/src/BattleGrid/Terrain.php
@@ -4,16 +4,49 @@
 namespace EverCraft\BattleGrid;
 
 
+/**
+ * Class Terrain
+ * @package EverCraft\BattleGrid
+ */
 class Terrain
 {
     const NORMAL = 'Normal';
-    const HIGH = 'High';
-    const LOW = 'Low';
-    protected $height;
+    const HIGH   = 'High';
+    const LOW    = 'Low';
+    const DIFFICULT = 'Difficult';
 
+    /**
+     * @var string
+     */
+    protected $height;
+    /**
+     * @var string
+     */
+    protected $quality;
+
+    /**
+     * Terrain constructor.
+     */
     public function __construct()
     {
         $this->height = self::NORMAL;
+        $this->quality = self::NORMAL;
+    }
+
+    /**
+     * @return string
+     */
+    public function getQuality(): string
+    {
+        return $this->quality;
+    }
+
+    /**
+     * @param string $quality
+     */
+    public function setQuality(string $quality): void
+    {
+        $this->quality = $quality;
     }
 
     /**
